ee07b3fc58f186e05754b065fefc55cfb1b2b443
Anjanique <anjaniquemackey@gmail.com>
2023-08-05 21:29:46 -0700

    bootstrap frontend

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-import express, { Express } from 'express';
+import express from 'express';
 import Mongoose from "mongoose";
 import morgan from 'morgan';
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
 
-const app: Express = express();
-
-// middleware
-
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV !== 'production') {
     morgan('dev');
 }
 
+// Middleware
+
+const app = express();
 app.use(express.json());
 
-// only start server if connecton to db was successful
 function startServer() {
     if (!process.env.MONGO_URI) {
         throw new Error("No database detected.");
@@ -24,7 +22,7 @@ function startServer() {
         .then(() => {
             app.listen(PORT, () => {
                 console.log(`Server listening on port: ${PORT}`);
-            })
+            });
         })
         .catch(err => console.log(err));
 }
